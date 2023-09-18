b5babde4ef8d6ac3db1a17405293ae2579bbc08d
MichaelRice1 <m.rice11@universityofgalway.ie>
2023-09-18 17:19:10 +0100

    Added Pages Functionality

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PIL import Image
 import sqlite3
 from io import BytesIO
-import base64
 
 
 st.set_page_config(page_title="grAIde", page_icon=":camera:", layout = "wide")
@@ -16,7 +15,8 @@ target2 = Image.open("images/target2.png")
 after2 = Image.open("images/after2.gif")
 
 
-img_cam = Image.open("images/camera.png")  
+img_cam = Image.open("images/camera.png") 
+logo = Image.open("images/logo.png") 
 
 conn = sqlite3.connect('images_db.db')
 cursor = conn.cursor()
@@ -32,53 +32,26 @@ conn.commit()
 def clear_database():
     cursor.execute("DELETE FROM images")
     conn.commit()
+    conn.close()
     # Close the database connection
     
 
+with st.container():
+    logo_column, navmenu_column = st.columns(2)
+    with logo_column:
+        resize_logo = logo.resize([350,150])
+        st.image(resize_logo)
+    with navmenu_column:
+        page = st.selectbox("", ("Home", "About","Samples", "Photo Grading","Video Grading"))
        
-       
+if page == "Home":
+    st.write("a side project by a 4th Year Computer Engineering Student.")
 
-
-with st.container():
-        st.header("grAIde")
-        st.subheader("Hello, this is a colour grading engine that uses AI to match the colour grade of any picture or video. By Michael. :) :camera:")
-        st.write("[Learn More > ] (https://github.com/MichaelRice1)")
-
-with st.container():
-        st.write("-----")
-        files_column, imgs_column, button_column = st.columns(3)
-        with files_column:
-            st.header("Upload files for Colour Grading Here")
-            uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg","mp4"])
-        with button_column:
-            imgs_button = st.button("Press to display images from database")
-            clear_button = st.button("Press to clear all images from database ")
-            if clear_button:
-                  clear_database 
-        with imgs_column:
-            if uploaded_image:
-                image = Image.open(uploaded_image)
-                st.image(image, caption='Uploaded Image', use_column_width=True)
-                img_bytes = BytesIO()
-                image.save(img_bytes, format="PNG")
-                img_data = img_bytes.getvalue()
-                cursor.execute("INSERT INTO images (image_data) VALUES (?)", (img_data,))
-                conn.commit()
-                st.success('Image uploaded and stored in the database!')
-            if imgs_button:     
-                st.header('Images Stored in the Database')
-                cursor.execute("SELECT id, image_data FROM images")
-                images = cursor.fetchall()
-                for image_id, img_data in images:
-    # Display images by converting bytes data back to image
-                    img = Image.open(BytesIO(img_data))
-                    st.image(img, caption=f"Image ID: {image_id}", use_column_width=True)
-                
-                
-            
-
-
-with st.container():
+if page == "About":
+    with st.container():
+            st.subheader("A colour grading engine using AI to match the colour grade of any picture or video. By Michael  :)")
+            st.write("[Learn More > ] (https://github.com/MichaelRice1)")
+    with st.container():
         st.write("---")
         left_column, right_column = st.columns(2)
         with left_column:
@@ -92,30 +65,75 @@ with st.container():
 
                 """
             )
-with right_column:
+        with right_column:
             st.image(img_cam)
 
-with st.container():
-            st.write("---")
-            st.header("Examples")
-            st.write("##")
-            before_column, target_column, after_column = st.columns(3)
-            with before_column:
-                   st.markdown("<h1 style='text-align: center;'>Before</h1>", unsafe_allow_html=True)
-                   st.image(before1)
-                   resized_image1 = before2.resize(before1.size)
-                   st.image(resized_image1)
-            with target_column:
-                   st.markdown("<h1 style='text-align: center;'>Target</h1>", unsafe_allow_html=True)
-                   st.image(target1)
-                   resized_image2 = target2.resize(before1.size)
-                   st.image(resized_image2)
-            with after_column:
-                   st.markdown("<h1 style='text-align: center;'>After</h1>", unsafe_allow_html=True)
-                   st.image(after1)
-                   resized_image3 = after2.resize(after1.size)
-                   st.image(resized_image3)
-                   
+if page == "Samples":
+    with st.container():
+        st.write("---")
+        st.header("Examples")
+        st.write("##")
+        before_column, target_column, after_column = st.columns(3)
+        with before_column:
+            st.markdown("<h1 style='text-align: center;'>Before</h1>", unsafe_allow_html=True)
+            st.image(before1)
+            resized_image1 = before2.resize(before1.size)
+            st.image(resized_image1)
+        with target_column:
+            st.markdown("<h1 style='text-align: center;'>Target</h1>", unsafe_allow_html=True)
+            st.image(target1)
+            resized_image2 = target2.resize(before1.size)
+            st.image(resized_image2)
+        with after_column:
+            st.markdown("<h1 style='text-align: center;'>After</h1>", unsafe_allow_html=True)
+            st.image(after1)
+            resized_image3 = after2.resize(after1.size)
+            st.image(resized_image3)
 
 
-
+if page == "Photo Grading":
+    with st.container():
+            st.write("-----")
+            files_column, imgs_column, button_column = st.columns(3)
+            with files_column:
+                st.header("Upload images for Colour Grading Here")
+                uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+            with button_column:
+                imgs_button = st.button("Press to display images from database")
+                clear_button = st.button("Press to clear all images from database ")
+                if clear_button:
+                    clear_database 
+            with imgs_column:
+                if uploaded_image:
+                    image = Image.open(uploaded_image)
+                    st.image(image, caption='Uploaded Image', use_column_width=True)
+                    img_bytes = BytesIO()
+                    image.save(img_bytes, format="PNG")
+                    img_data = img_bytes.getvalue()
+                    cursor.execute("INSERT INTO images (image_data) VALUES (?)", (img_data,))
+                    conn.commit()
+                    st.success('Image uploaded and stored in the database!')
+                if imgs_button:     
+                    st.header('Images Stored in the Database')
+                    cursor.execute("SELECT id, image_data FROM images")
+                    images = cursor.fetchall()
+                    for image_id, img_data in images:
+        # Display images by converting bytes data back to image
+                        img = Image.open(BytesIO(img_data))
+                        st.image(img, caption=f"Image ID: {image_id}", use_column_width=True)
+                
+if page == "Video Grading":
+     with st.container():
+            st.write("-----")
+            files_column, vids_column, button_column = st.columns(3)
+            with files_column:
+                st.header("Upload videos for Colour Grading Here")
+                uploaded_video = st.file_uploader("Upload a video", type=["mp4"])
+            with vids_column:
+                if uploaded_video is not None:
+                    st.video(uploaded_video, format="video/mp4")
+                else:
+                    st.warning("Please upload a video")
+            with button_column:
+                grAIde_vids_button = st.button("Press to grAIde.")
+                 
